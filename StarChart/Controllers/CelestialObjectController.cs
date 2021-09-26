fd6c92063935990243fe542875630f975bec50d1
darrennellis91 <nellis.1991@live.co.uk>
2021-09-26 20:39:48 +0100

    added celesital controller with context

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    public class CelestialObjectController : ControllerBase
+    {
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ApplicationDbContext _context { get; }
+    }
+}
