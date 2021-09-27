f1af311acc7f1dbe55e0f003737ef2a2ff0a7cf6
darrennellis91 <nellis.1991@live.co.uk>
2021-09-27 20:51:25 +0100

    added private accessor to _context

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -7,14 +7,15 @@ using StarChart.Models;
 namespace StarChart.Controllers
 {
     [Route("")]
+    [ApiController]
     public class CelestialObjectController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
-        }
-
-        public readonly ApplicationDbContext _context;
+        }   
 
         [HttpGet("{id:int}", Name = "GetById")]
         public IActionResult GetById(int id)
